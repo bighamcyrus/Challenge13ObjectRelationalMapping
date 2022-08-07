e76256a22ec28b36d5f9f247eee4892328fa94a7
Cyrus Bigham <bighamcyrus@gmail.com>
2022-08-06 17:52:47 -0700

    working on connecting to insomnia

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,19 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
-
-// const sequelize = new Sequelize(
-
-//   // database name
-//   process.env.DATABASE_NAME,
-//   // user
-//   process.env.USER, 
-//   // password
-//   process.env.PASSWORD,
-//   {
-//     host: 'localhost',
-//     dialect:'mysql',
-//     port: 3306
-//   }
-// );
-// below is the starter code connection for sequilize / not sure if this was right!!!???
- const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DATABASE_NAME, process.env.USER, process.env.DB_PASSWORD, {
+ let sequelize;
+  
+ if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+ } else {
+  sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+  }
 
 module.exports = sequelize;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
       // Add Book as a second model to JOIN with
       include: [{ model: Tag }, { model: Category }],
     });
-    res.status(200).json(readerData);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -32,7 +32,7 @@ router.get('/:id', async (req, res) => {
       return;
     }
 
-    res.status(200).json(readerData);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
